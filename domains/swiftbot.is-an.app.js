cf503a6bfcbd3cf8087b56cca6a3eb16b5ad92da
celestialPluse <140946993+celestialPluse@users.noreply.github.com>
2024-04-04 04:01:10 +0300

    Create swiftbot.is-an.app.js

---
+++ domains/swiftbot.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: '...', // describe your project in this field
+  domain: 'is-an.app', // aka "root-domain". select between '1bt.uk' and 'is-an.app'
+  subdomain: 'swiftbot', // desired subdomain name
+  owner: {
+    repo: 'https://github.com/celestialPluse/free-domains',
+    email: 'fyt.official12@gmail.come',
+  },
+  record: {
+    A: ['193.23.161.211'],
+  },
+ 
+})
